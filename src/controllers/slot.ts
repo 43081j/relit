abc6be3e7d48d6db22d2b7f14bedc0f8015c2cc9
43081j <43081j@users.noreply.github.com>
2023-03-12 22:44:02 +0000

    feat (slot): rework how slotcontroller works
    This reworks the slot controller such that it deals with _multiple_
    slots rather than a single slot.
    
    This means `addListener` must now be given a slot to listen on:
    
    ```ts
    ctrl.addListener('slotname', 'selector', fn);
    ```
    
    Similarly, the new `has` method behaves the same:
    
    ```ts
    // true if the slotname slot has elements
    ctrl.has('slotname');
    ```

--- src/controllers/slot.ts
+++ src/controllers/slot.ts
@@ -1,7 +1,5 @@
-import type {ReactiveControllerHost} from 'lit';
+import type {ReactiveControllerHost, ReactiveController} from 'lit';
 import type {Ref} from 'lit/directives/ref.js';
-import {createRef} from 'lit/directives/ref.js';
-import {ElementTrackingController} from '../_internal/elementTracking.js';
 
 /**
  * Determines if a node is a slot element
@@ -12,9 +10,31 @@ function isSlotElement(node: Element): node is HTMLSlotElement {
   return node.nodeName === 'SLOT';
 }
 
+/**
+ * Determines if a given slot string/ref matches the specified slot
+ * @param {HTMLSlotElement} node Node to test against
+ * @param {string|Ref} matcher Ref or selector to match
+ * @return {boolean}
+ */
+function slotMatches(
+  node: HTMLSlotElement,
+  matcher: string | Ref<HTMLSlotElement>
+): boolean {
+  if (matcher === 'default' && !node.hasAttribute('name')) {
+    return true;
+  }
+
+  if (typeof matcher !== 'string') {
+    return matcher.value === node;
+  }
+
+  return matcher === node.name;
+}
+
 type SlotListenerCallback<T extends Element> = (node: T) => void;
 
 interface SlotListener {
+  slot: string | Ref<HTMLSlotElement>;
   selector: string;
   callback: SlotListenerCallback<Element>;
 }
@@ -22,56 +42,98 @@ interface SlotListener {
 /**
  * Helpers for dealing with slotted content
  */
-export class SlotController extends ElementTrackingController {
+export class SlotController implements ReactiveController {
+  private __host: ReactiveControllerHost & Element;
+  private __hasCalled: boolean = false;
+  private __listeners: Set<SlotListener> = new Set<SlotListener>();
+
   /**
-   * Gets the current ref
-   * @return {Ref}
+   * Gets the slot elements in the current host
+   * @return {HTMLSlotElement[]}
    */
-  public get ref(): Ref {
-    // Casting here to drop the nullability
-    return this._ref as Ref;
+  private get __slots(): HTMLSlotElement[] {
+    if (!this.__host.shadowRoot) {
+      return [];
+    }
+
+    return [...this.__host.shadowRoot.querySelectorAll('slot')];
   }
 
   /**
    * @param {ReactiveControllerHost} host Host to attach to
-   * @param {Ref} ref Ref to observe rather than the host element
    */
-  public constructor(
-    host: ReactiveControllerHost & Element,
-    ref: Ref = createRef()
-  ) {
-    super(host, ref);
+  public constructor(host: ReactiveControllerHost & Element) {
+    this.__host = host;
 
     host.addController(this);
   }
 
-  private __listeners: Set<SlotListener> = new Set<SlotListener>();
+  /**
+   * Determines if a given slot has contents in the host
+   * @param {string|Ref} slot Slot to test against, * for all slots
+   * @param {string} selector Selector to test
+   * @return {boolean}
+   */
+  public has(slot: string | Ref<HTMLSlotElement>, selector?: string): boolean {
+    const slotName = typeof slot === 'string' ? slot : slot.value?.name;
+
+    this.__hasCalled = true;
+
+    if (!slotName) {
+      return false;
+    }
+
+    const slots = this.__slots.filter((slot) => {
+      return (
+        slotName === '*' ||
+        (!slot.hasAttribute('name') && slotName === 'default') ||
+        slot.name === slotName
+      );
+    });
+    const children = slots.reduce<Element[]>((accum, slot) => {
+      for (const child of slot.assignedElements()) {
+        accum.push(child);
+      }
+      return accum;
+    }, []);
+
+    if (!selector) {
+      return children.length > 0;
+    }
+
+    return children.some((child) => child.matches(selector));
+  }
 
   /**
    * Binds a function to be called any time elements matching the specified
-   * selector are slotted into the slot
+   * selector are slotted into a given slot
+   * @param {string|Ref} slot Slot to observe, * for all slots
    * @param {string} selector Query selector to filter elements by. Set this
    * to `*` for all elements
    * @param {SlotListenerCallback} callback Callback to call
    * @return {void}
    */
   public addListener<T extends keyof HTMLElementTagNameMap>(
+    slot: string | Ref<HTMLSlotElement>,
     selector: T,
     callback: SlotListenerCallback<HTMLElementTagNameMap[T]>
   ): void;
 
   /** @inheritdoc */
   public addListener(
+    slot: string | Ref<HTMLSlotElement>,
     selector: string,
     callback: SlotListenerCallback<Element>
   ): void;
 
   /** @inheritdoc */
   public addListener(
+    slot: string | Ref<HTMLSlotElement>,
     selector: string,
     callback: SlotListenerCallback<Element>
   ): void {
     this.__listeners.add({
+      slot,
       selector,
       callback
     });
@@ -79,38 +141,48 @@ export class SlotController extends ElementTrackingController {
 
   /**
    * Fired when a slot change event occurred
+   * @param {Event} ev Event fired
    * @return {void}
    */
-  private __onSlotChanged = (): void => {
-    const element = this._element;
+  private __onSlotChanged = (ev: Event): void => {
+    const element = ev.target as Element;
 
-    if (!element || !isSlotElement(element)) {
+    if (!isSlotElement(element)) {
       return;
     }
 
     const nodes = element.assignedElements();
 
     for (const listener of this.__listeners) {
+      if (listener.slot !== '*' && !slotMatches(element, listener.slot)) {
+        continue;
+      }
+
       for (const node of nodes) {
         if (listener.selector === '*' || node.matches(listener.selector)) {
           listener.callback(node);
         }
       }
     }
+
+    if (this.__hasCalled) {
+      this.__host.requestUpdate();
+    }
   };
 
   /** @inheritdoc */
-  protected override _onElementChanged(prevElement: Element | undefined): void {
-    super._onElementChanged(prevElement);
+  public hostConnected(): void {
+    this.__host.shadowRoot?.addEventListener(
+      'slotchange',
+      this.__onSlotChanged
+    );
+  }
 
-    if (prevElement) {
-      prevElement.removeEventListener('slotchange', this.__onSlotChanged);
-    }
-
-    const element = this._element;
-
-    if (element) {
-      element.addEventListener('slotchange', this.__onSlotChanged);
-    }
+  /** @inheritdoc */
+  public hostDisconnected(): void {
+    this.__host.shadowRoot?.removeEventListener(
+      'slotchange',
+      this.__onSlotChanged
+    );
   }
 }
