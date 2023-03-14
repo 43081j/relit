8dabd8411c152cffa3a27b7e33975ce976582e30
James Garbutt <43081j@users.noreply.github.com>
2023-03-14 21:16:41 +0000

    feat (slot): rework how slotcontroller works (#13)
    This reworks the slot controller such that it deals with _multiple_
    slots rather than a single slot.
    
    This means `addListener` must now be given a slot to listen on:
    
    ```ts
    ctrl.addListener('slotname', 'selector', fn);
    ```
    
    Similarly, the new `has` method behaves the same:
    
    ```ts
    // true if the slotname slot has elements
    ctrl.has('slotname');
    ```

--- src/test/controllers/slot_test.ts
+++ src/test/controllers/slot_test.ts
@@ -3,15 +3,13 @@ import '../util.js';
 import {html} from 'lit';
 import * as hanbi from 'hanbi';
 import * as assert from 'uvu/assert';
-import {createRef, ref} from 'lit/directives/ref.js';
-import type {Ref} from 'lit/directives/ref.js';
 import {SlotController} from '../../main.js';
 import type {TestElement} from '../util.js';
+import {ref, createRef} from 'lit/directives/ref.js';
 
 suite('SlotController', () => {
   let element: TestElement;
   let controller: SlotController;
-  let elementRef: Ref<HTMLElement>;
 
   setup(() => {
     element = document.createElement('test-element');
@@ -22,10 +20,9 @@ suite('SlotController', () => {
   });
 
   setup(async () => {
-    elementRef = createRef<HTMLElement>();
-    controller = new SlotController(element, elementRef);
+    controller = new SlotController(element);
     element.controllers.push(controller);
-    element.template = () => html`<slot ${ref(elementRef)}></slot>`;
+    element.template = () => html`<slot></slot>`;
     document.body.appendChild(element);
     await element.updateComplete;
   });
@@ -34,7 +31,7 @@ suite('SlotController', () => {
     test('calls listener with slotted elements', async () => {
       const spy = hanbi.spy<(node: Element) => void>();
 
-      controller.addListener('*', spy.handler);
+      controller.addListener('*', '*', spy.handler);
 
       element.innerHTML = '<span>foo</span><div>bar</div>';
       await element.updateComplete;
@@ -51,58 +48,293 @@ suite('SlotController', () => {
       assert.is(secondCall.args[0], divNode);
     });
 
-    suite('options.selector', () => {
-      test('simple selector matches elements', async () => {
-        const spy = hanbi.spy<(node: HTMLSpanElement) => void>();
+    test('does not call listener for other slot', async () => {
+      const spy = hanbi.spy<(node: Element) => void>();
 
-        controller.addListener('span', spy.handler);
+      element.template = () => html`
+        <slot name="test"></slot>
+        <slot name="test2"></slot>
+      `;
+      await element.updateComplete;
 
-        element.innerHTML = '<span>foo</span><div>bar</div>';
-        await element.updateComplete;
+      controller.addListener('test', '*', spy.handler);
 
-        const spanNode = element.querySelector('span')!;
+      element.innerHTML = `
+        <span slot="test2">foo</span>
+      `;
+      await element.updateComplete;
 
-        assert.is(spy.callCount, 1);
+      assert.is(spy.callCount, 0);
+    });
 
-        const firstCall = spy.getCall(0);
+    test('simple selector matches elements', async () => {
+      const spy = hanbi.spy<(node: HTMLSpanElement) => void>();
 
-        assert.is(firstCall.args[0], spanNode);
-      });
+      controller.addListener('*', 'span', spy.handler);
 
-      test('complex selector matches elements', async () => {
-        const spy = hanbi.spy<(node: Element) => void>();
+      element.innerHTML = '<span>foo</span><div>bar</div>';
+      await element.updateComplete;
 
-        controller.addListener('span,div', spy.handler);
+      const spanNode = element.querySelector('span')!;
 
-        element.innerHTML = '<span>foo</span><div>bar</div><h1>baz</h1>';
-        await element.updateComplete;
+      assert.is(spy.callCount, 1);
 
-        const spanNode = element.querySelector('span')!;
-        const divNode = element.querySelector('div')!;
+      const firstCall = spy.getCall(0);
 
-        assert.is(spy.callCount, 2);
+      assert.is(firstCall.args[0], spanNode);
+    });
 
-        const firstCall = spy.getCall(0);
-        const secondCall = spy.getCall(1);
+    test('complex selector matches elements', async () => {
+      const spy = hanbi.spy<(node: Element) => void>();
 
-        assert.is(firstCall.args[0], spanNode);
-        assert.is(secondCall.args[0], divNode);
-      });
+      controller.addListener('*', 'span,div', spy.handler);
+
+      element.innerHTML = '<span>foo</span><div>bar</div><h1>baz</h1>';
+      await element.updateComplete;
+
+      const spanNode = element.querySelector('span')!;
+      const divNode = element.querySelector('div')!;
+
+      assert.is(spy.callCount, 2);
+
+      const firstCall = spy.getCall(0);
+      const secondCall = spy.getCall(1);
+
+      assert.is(firstCall.args[0], spanNode);
+      assert.is(secondCall.args[0], divNode);
     });
 
     test('observes element changes', async () => {
       const spy = hanbi.spy<(node: Element) => void>();
 
-      controller.addListener('*', spy.handler);
+      controller.addListener('*', '*', spy.handler);
 
-      element.template = () =>
-        html`<div><slot ${ref(elementRef)}></slot></div>`;
+      element.template = () => html`<div><slot></slot></div>`;
       await element.updateComplete;
 
       element.innerHTML = '<span>foo</span><div>bar</div>';
       await element.updateComplete;
 
       assert.is(spy.callCount, 2);
+    });
+
+    test('can specify exact slot by name', async () => {
+      const spy = hanbi.spy<(node: Element) => void>();
+
+      element.template = () => html`<slot name="test"></slot>`;
+      await element.updateComplete;
+
+      controller.addListener('test', '*', spy.handler);
+
+      element.innerHTML = '<span slot="test">foo</span><div>bar</div>';
+      await element.updateComplete;
+
+      const spanNode = element.querySelector('span')!;
+
+      assert.is(spy.callCount, 1);
+
+      const firstCall = spy.getCall(0);
+
+      assert.is(firstCall.args[0], spanNode);
+    });
+
+    test('can specify exact slot by ref', async () => {
+      const spy = hanbi.spy<(node: Element) => void>();
+      const slotRef = createRef<HTMLSlotElement>();
+
+      element.template = () => html`
+        <slot name="test" ${ref(slotRef)}></slot>
+      `;
+      await element.updateComplete;
+
+      controller.addListener(slotRef, '*', spy.handler);
+
+      element.innerHTML = '<span slot="test">foo</span><div>bar</div>';
+      await element.updateComplete;
+
+      const spanNode = element.querySelector('span')!;
+
+      assert.is(spy.callCount, 1);
+
+      const firstCall = spy.getCall(0);
+
+      assert.is(firstCall.args[0], spanNode);
+    });
+
+    test('can specify default slot', async () => {
+      const spy = hanbi.spy<(node: Element) => void>();
+
+      controller.addListener('[default]', '*', spy.handler);
+
+      element.innerHTML = '<span>foo</span><div>bar</div>';
+      await element.updateComplete;
+
+      const spanNode = element.querySelector('span')!;
+      const divNode = element.querySelector('div')!;
+
+      assert.is(spy.callCount, 2);
+
+      const firstCall = spy.getCall(0);
+      const secondCall = spy.getCall(1);
+
+      assert.is(firstCall.args[0], spanNode);
+      assert.is(secondCall.args[0], divNode);
+    });
+  });
+
+  suite('has', () => {
+    test('false if no slot name', async () => {
+      assert.is(controller.has(''), false);
+    });
+
+    test('true if slot has elements', async () => {
+      element.template = () => html`
+        <slot name="test"></slot>
+      `;
+      await element.updateComplete;
+
+      assert.is(controller.has('test'), false);
+
+      element.innerHTML = `
+        <span slot="test">foo</span>
+      `;
+
+      assert.is(controller.has('test'), true);
+    });
+
+    test('can pass slot as ref', async () => {
+      const slotRef = createRef<HTMLSlotElement>();
+
+      element.template = () => html`
+        <slot name="test" ${ref(slotRef)}></slot>
+      `;
+      await element.updateComplete;
+
+      assert.is(controller.has(slotRef), false);
+
+      element.innerHTML = `
+        <span slot="test">foo</span>
+      `;
+
+      assert.is(controller.has(slotRef), true);
+    });
+
+    test('true if any slot has elements (slot=*)', async () => {
+      element.template = () => html`
+        <slot name="test"></slot>
+        <slot></slot>
+      `;
+      await element.updateComplete;
+
+      assert.is(controller.has('*'), false);
+
+      element.innerHTML = `
+        <span slot="test">foo</span>
+        <span>bar</span>
+      `;
+
+      assert.is(controller.has('*'), true);
+    });
+
+    test('true if any slot has matching elements (slot=*)', async () => {
+      element.template = () => html`
+        <slot name="test"></slot>
+        <slot></slot>
+      `;
+      await element.updateComplete;
+
+      assert.is(controller.has('*', 'span'), false);
+
+      element.innerHTML = `
+        <div slot="test">foo</div>
+        <span>bar</span>
+      `;
+
+      assert.is(controller.has('*', 'span'), true);
+    });
+
+    test('true if slot has matching elements', async () => {
+      element.template = () => html`<slot name="test"></slot>`;
+      await element.updateComplete;
+
+      element.innerHTML = '<span slot="test">foo</span>';
+
+      assert.is(controller.has('test', 'span'), true);
+      assert.is(controller.has('test', 'div'), false);
+    });
+
+    test('true if default slot has elements', () => {
+      assert.is(controller.has('[default]'), false);
+
+      element.innerHTML = '<span>foo</span>';
+
+      assert.is(controller.has('[default]'), true);
+    });
+
+    test('false if not the specified slot', async () => {
+      element.template = () => html`
+        <slot name="test"></slot>
+        <slot name="test2"></slot>
+      `;
+      await element.updateComplete;
+
+      assert.is(controller.has('test'), false);
+
+      element.innerHTML = `
+        <div slot="test2">foo</div>
+      `;
+
+      assert.is(controller.has('test'), false);
+    });
+
+    test('triggers re-render when condition changes', async () => {
+      element.template = () => html`
+        <slot></slot>
+        <span>${String(controller.has('*'))}</span>
+      `;
+      await element.updateComplete;
+
+      const span = element.shadowRoot!.querySelector('span')!;
+
+      assert.is(span.textContent, 'false');
+
+      element.innerHTML = '<span>foo</span>';
+      await element.updateComplete;
+      await element.updateComplete;
+
+      assert.is(span.textContent, 'true');
+    });
+
+    test('handles re-renders with conditional has() calls', async () => {
+      let flag = true;
+
+      element.template = () => html`
+        <slot></slot>
+        <span>${flag ? String(controller.has('*')) : 'nothing'}</span>
+      `;
+      await element.updateComplete;
+
+      const span = element.shadowRoot!.querySelector('span')!;
+
+      assert.is(span.textContent, 'false');
+
+      flag = false;
+      element.requestUpdate();
+      await element.updateComplete;
+
+      assert.is(span.textContent, 'nothing');
+
+      flag = true;
+      element.requestUpdate();
+      await element.updateComplete;
+
+      assert.is(span.textContent, 'false');
+
+      element.innerHTML = '<span>foo</span>';
+      await element.updateComplete;
+      await element.updateComplete;
+
+      assert.is(span.textContent, 'true');
     });
   });
 });
