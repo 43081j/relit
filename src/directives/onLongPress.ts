04fbb7af9ff76b3e7748b14cfd7d5489328d0c48
Valentin Degenne <vdegenne@gmail.com>
2023-06-06 13:40:10 +0200

    remove private fields

--- src/directives/onLongPress.ts
+++ src/directives/onLongPress.ts
@@ -9,11 +9,11 @@ import {
 } from 'lit/directive.js';
 
 class LongPressDirective extends Directive {
-	#element!: Element;
-	#fireTimeout?: number;
+	__element!: Element;
+	__fireTimeout?: number;
 
-	#longPressCallback?: Function;
-	#cancelTimeoutMs?: number;
+	__longPressCallback?: Function;
+	__cancelTimeoutMs?: number;
 
 	constructor(partInfo: PartInfo) {
 		super(partInfo);
@@ -25,36 +25,40 @@ class LongPressDirective extends Directive {
 			);
 		}
 
-		this.#element = (partInfo as ElementPart).element;
+		this.__element = (partInfo as ElementPart).element;
 	}
 
 	render(callback: (event?: Event) => void, cancelTimeoutMs = 2000) {
-		this.#abort();
-		this.#longPressCallback = callback;
-		this.#cancelTimeoutMs = cancelTimeoutMs;
-		this.#element.addEventListener('mousedown', this.#initiateTimeout);
-		this.#element.addEventListener('mouseup', this.#cancelTimeout);
-		this.#element.addEventListener('mouseleave', this.#cancelTimeout);
+		this.__abort();
+		this.__longPressCallback = callback;
+		this.__cancelTimeoutMs = cancelTimeoutMs;
+		this.__bindInitialTimeout = this.__initiateTimeout.bind(this)
+		this.__element.addEventListener('mousedown', this.__bindInitialTimeout);
+		this.__bindCancelTimeout = this.__cancelTimeout.bind(this)
+		this.__element.addEventListener('mouseup', this.__bindCancelTimeout);
+		this.__element.addEventListener('mouseleave', this.__bindCancelTimeout);
 		return nothing;
 	}
 
-	#initiateTimeout() {
-		this.#fireTimeout = setTimeout(() => {
-			this.#longPressCallback?.(this.#element);
-		}, this.#cancelTimeoutMs);
+	__bindInitialTimeout!: (e: Event) => void;
+	__initiateTimeout() {
+		this.__fireTimeout = setTimeout(() => {
+			this.__longPressCallback?.(this.__element);
+		}, this.__cancelTimeoutMs);
 	}
-	#cancelTimeout() {
-		if (this.#fireTimeout) {
-			clearTimeout(this.#fireTimeout);
+	__bindCancelTimeout!: (e: Event) => void;
+	__cancelTimeout() {
+		if (this.__fireTimeout) {
+			clearTimeout(this.__fireTimeout);
 		}
 	}
 
-	#abort() {
-		this.#cancelTimeout();
+	__abort() {
+		this.__cancelTimeout();
 		// Are removeEventListener's needed here?
-		this.#element.removeEventListener('mousedown', this.#initiateTimeout);
-		this.#element.removeEventListener('mouseup', this.#cancelTimeout);
-		this.#element.removeEventListener('mouseleave', this.#cancelTimeout);
+		this.__element.removeEventListener('mousedown', this.__bindInitialTimeout);
+		this.__element.removeEventListener('mouseup', this.__bindCancelTimeout);
+		this.__element.removeEventListener('mouseleave', this.__bindCancelTimeout);
 	}
 }
 
