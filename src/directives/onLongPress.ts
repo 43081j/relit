ddd5c5bbab0d7e4eb4c5249191d461d2e2f64081
Valentin Degenne <vdegenne@gmail.com>
2023-06-06 16:52:03 +0200

    comments

--- src/directives/onLongPress.ts
+++ src/directives/onLongPress.ts
@@ -29,36 +29,82 @@ class LongPressDirective extends Directive {
 	}
 
 	render(callback: (event?: Event) => void, cancelTimeoutMs = 2000) {
+		// Cancel timeout/events and discard event listeners
 		this.__abort();
+
 		this.__longPressCallback = callback;
 		this.__cancelTimeoutMs = cancelTimeoutMs;
-		this.__bindInitialTimeout = this.__initiateTimeout.bind(this)
-		this.__element.addEventListener('mousedown', this.__bindInitialTimeout);
-		this.__bindCancelTimeout = this.__cancelTimeout.bind(this)
-		this.__element.addEventListener('mouseup', this.__bindCancelTimeout);
-		this.__element.addEventListener('mouseleave', this.__bindCancelTimeout);
+
+		// Because events were removed from __abort
+		// we create them again
+		this.__bindOnElementMouseDown = this.__onElementMouseDown.bind(this);
+		this.__element.addEventListener('mousedown', this.__bindOnElementMouseDown);
+		this.__bindOnElementMouseUp = this.__onElementMouseUp.bind(this);
+		this.__element.addEventListener('mouseup', this.__bindOnElementMouseUp);
+		this.__bindOnElementMouseLeave = this.__onElementMouseLeave.bind(this);
+		this.__element.addEventListener(
+			'mouseleave',
+			this.__bindOnElementMouseLeave
+		);
+
 		return nothing;
 	}
 
-	__bindInitialTimeout!: (e: Event) => void;
+	__onElementMouseDown() {
+		this.__initiateTimeout();
+	}
+	__bindOnElementMouseDown!: (event: Event) => void;
+
+	__onElementMouseUp(e: Event) {
+		// TODO: when the mouse is released and long press event
+		// was accepted, we should find a way to cancel the @click
+		// event listener if it exists.
+
+		this.__cancelTimeout();
+	}
+	__bindOnElementMouseUp!: (event: Event) => void;
+
+	__onElementMouseLeave() {
+		this.__cancelTimeout();
+	}
+	__bindOnElementMouseLeave!: (event: Event) => void;
+
+	/**
+	 * Start the long press timeout,
+	 * when the timeout runs out the user-defined callback is called.
+	 */
 	__initiateTimeout() {
 		this.__fireTimeout = setTimeout(() => {
 			this.__longPressCallback?.(this.__element);
 		}, this.__cancelTimeoutMs);
 	}
-	__bindCancelTimeout!: (e: Event) => void;
+
+	/**
+	 * Cancel the long press timeout.
+	 * This function is called when the user release the mouse
+	 * or when the mouse leave the element.
+	 */
 	__cancelTimeout() {
 		if (this.__fireTimeout) {
 			clearTimeout(this.__fireTimeout);
 		}
 	}
 
+	/**
+	 * Abort the long press timeout on special occasions.
+	 */
 	__abort() {
+		// TODO: should call this.__abort() when the template is removed/destroyed?
 		this.__cancelTimeout();
-		// Are removeEventListener's needed here?
-		this.__element.removeEventListener('mousedown', this.__bindInitialTimeout);
-		this.__element.removeEventListener('mouseup', this.__bindCancelTimeout);
-		this.__element.removeEventListener('mouseleave', this.__bindCancelTimeout);
+		this.__element.removeEventListener(
+			'mousedown',
+			this.__bindOnElementMouseDown
+		);
+		this.__element.removeEventListener('mouseup', this.__bindOnElementMouseUp);
+		this.__element.removeEventListener(
+			'mouseleave',
+			this.__bindOnElementMouseLeave
+		);
 	}
 }
 
